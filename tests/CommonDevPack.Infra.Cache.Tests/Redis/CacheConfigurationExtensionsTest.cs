ab5bb0a2b39a76124324899497979a76fd16cb36
robertocamara <robertocamara@outlook.com.br>
2022-03-12 18:44:20 -0300

    Add test for CacheConfigurationExtensions class

---
+++ tests/CommonDevPack.Infra.Cache.Tests/Redis/CacheConfigurationExtensionsTest.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CommonDevPack.Infra.Cache.Tests.Redis;
+
+
+[Collection(nameof(CacheConfigurationExtensionsTestFixture))]
+public class CacheConfigurationExtensionsTest
+{
+    private readonly CacheConfigurationExtensionsTestFixture _cacheConfigurationTestFixture;
+
+    public CacheConfigurationExtensionsTest(CacheConfigurationExtensionsTestFixture cacheConfigurationTestFixture)
+    {
+        _cacheConfigurationTestFixture = cacheConfigurationTestFixture;
+    }
+
+    [Fact(DisplayName = nameof(AddRedisConfigureConnectionStringNull))]
+    [Trait("Infra.Cache", "AddRedisConfigureConnectionStringNull")]
+    public void AddRedisConfigureConnectionStringNull()
+    {
+        var host = _cacheConfigurationTestFixture.CreateDefaultBuilder("appsettings-fail.json");
+
+        var redisConfiguration = _cacheConfigurationTestFixture.GetRedisConfigurationInstance(host);
+
+        redisConfiguration.Value.ConnectionString.Should().BeNull();
+        
+    }
+
+    [Fact(DisplayName = nameof(AddRedisConfigureConnectionStringNotNull))]
+    [Trait("Infra.Cache", "AddRedisConfigureConnectionStringNotNull")]
+    public void AddRedisConfigureConnectionStringNotNull()
+    {
+        var host = _cacheConfigurationTestFixture.CreateDefaultBuilder("appsettings.json");
+
+        var redisConfiguration = _cacheConfigurationTestFixture.GetRedisConfigurationInstance(host);
+
+        redisConfiguration.Value.ConnectionString.Should().NotBeNull();
+    }
+
+    [Fact(DisplayName = nameof(AddRedisConfigureCustomSectionKeyValid))]
+    [Trait("Infra.Cache", "AddRedisConfigureCustomSectionKeyValid")]
+    public void AddRedisConfigureCustomSectionKeyValid()
+    {
+        var host = _cacheConfigurationTestFixture.CreateDefaultBuilder("appsettings-customsection.json", "ConfigurationServices:Redis");
+
+        var redisConfiguration = _cacheConfigurationTestFixture.GetRedisConfigurationInstance(host);
+
+        redisConfiguration.Value.ConnectionString.Should().NotBeNull();
+    }
+
+    [Fact(DisplayName = nameof(AddRedisConfigureCustomSectionKeyInvalid))]
+    [Trait("Infra.Cache", "AddRedisConfigureCustomSectionKeyInvalid")]
+    public void AddRedisConfigureCustomSectionKeyInvalid()
+    {
+        var host = _cacheConfigurationTestFixture.CreateDefaultBuilder("appsettings-fail.json", "InvalidSection");
+
+        var redisConfiguration = _cacheConfigurationTestFixture.GetRedisConfigurationInstance(host);
+
+        redisConfiguration.Value.ConnectionString.Should().BeNull();
+    }
+}
---
+++ tests/CommonDevPack.Infra.Cache.Tests/Redis/CacheConfigurationExtensionsTestFixture.cs
@@ -0,0 +1,36 @@
+﻿using CommonDevPack.Infra.Cache.Redis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System;
+using Xunit;
+
+namespace CommonDevPack.Infra.Cache.Tests.Redis;
+public class CacheConfigurationExtensionsTestFixture
+{
+    public IConfiguration GetConfiguration(string appsettings) =>
+           new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(appsettings, false, true)
+                .Build();
+
+    public IHost CreateDefaultBuilder(string appsettings, string? customSectionKey = null) =>
+        Host.CreateDefaultBuilder()
+            .ConfigureServices((services) =>
+            {
+                if (string.IsNullOrWhiteSpace(customSectionKey))
+                    services.AddRedisConfigure(GetConfiguration(appsettings));
+                else
+                    services.AddRedisConfigure(GetConfiguration(appsettings), customSectionKey);
+            })
+            .Build();
+
+    public IOptions<RedisConfiguration> GetRedisConfigurationInstance(IHost host) => 
+        host.Services.GetService<IOptions<RedisConfiguration>>();
+}
+
+[CollectionDefinition(nameof(CacheConfigurationExtensionsTestFixture))]
+public class CacheConfigurationExtensionsTestFixtureCollection
+    : ICollectionFixture<CacheConfigurationExtensionsTestFixture>
+{ }
